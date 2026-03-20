a9ae45d0355ebc0f4abd8f34ccfe250de252e5fe
ste2000it-developer <ste2000.it@gmail.com>
2026-03-20 22:20:12 +0700

    Update service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = "ste2000-attendance-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./attendance.html",
+  "./style.css",
+  "./attendance.css",
+  "./app.js",
+  "./attendance.js",
+  "./firebase.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(request)
+        .then((networkResponse) => {
+          const responseClone = networkResponse.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(request, responseClone);
+          });
+
+          return networkResponse;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
