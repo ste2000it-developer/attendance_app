6abb506b69fb80f6ed6e57c59e42cab47c6e8b3a
ste2000it-developer <ste2000.it@gmail.com>
2026-03-23 02:56:05 +0000

    time

--- attendance.js
+++ attendance.js
@@ -162,7 +162,7 @@ function formatTime(date) {
 function formatCurrentTime(date) {
   return date.toLocaleTimeString("th-TH", {
     hour: "2-digit",
-    minute: "2-digit",
+    minute: "2-digit"
   });
 }
 
